cf58ffba392a13c04f575851b6cc4bf3484d553c
Ray Clanan <rclanan@utopianconcept.com>
2016-08-10 15:50:14 -0400

    Get test back up

--- src/app/specs.webpack.js
+++ src/app/specs.webpack.js
@@ -2,7 +2,7 @@
  * This file is an entry point for angular tests.
  * Avoids some weird issues when using webpack + angular.
  * */
-require('angular');
+
 require('./app.module');
 
 var testsContext = require.context('./', true, /.spec$/);
