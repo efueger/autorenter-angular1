c02198f08e29658d8bdf609270ad4148018ff8ee
brian wortman <bwortman@fusionalliance.com>
2016-08-16 19:14:05 -0400

    Pulled logDecorator out so that it can be independently tested. WIP.

--- src/app/logging/logging.config.js
+++ src/app/logging/logging.config.js
@@ -2,44 +2,48 @@
 
 var logging = require('./logging.module');
 
-function provide($provide, logApiProvider) {
-  var logDecorator = function $logDecorator($delegate) {
-    function logToApi(message, severity) {
-      // Do NOT use $http in the context of logging because it creates a circular dependency, initiates the digest
-      // loop, and makes logging to the API impossible if Angular itself if hosed.
-      var payload = {
-        message: message,
-        level: severity,
-        username: 'bill'
+var logDecorator = function($delegate, logApiProvider) {
+  function logToApi(message, severity) {
+    // Do NOT use $http in the context of logging because it creates a circular dependency, initiates the digest
+    // loop, and makes logging to the API impossible if Angular itself if hosed.
+    var payload = {
+      message: message,
+      level: severity,
+      username: 'bill'
+    };
+    logApiProvider.$get().send(JSON.stringify(payload));
+  }
+
+  function decorate() {
+    var messageIndex = 0;
+    var loggingMethods = ['info', 'warn', 'error']; // NOTE: 'log' and 'debug' were deliberately skipped.
+
+    loggingMethods.forEach(function decorateLogging(logLevel) {
+      var original = $delegate[logLevel];
+      $delegate[logLevel] = function logToApiDecorator() {
+        var args = [].slice.call(arguments);
+        original.apply($delegate, args);
+        logToApi(args[messageIndex], logLevel);
       };
-      logApiProvider.$get().send(JSON.stringify(payload));
-    }
+    });
 
-    function decorate() {
-      var messageIndex = 0;
-      var loggingMethods = ['info', 'warn', 'error']; // NOTE: 'log' and 'debug' were deliberately skipped.
+    // Special... only needed to support angular-mocks expectations.
+    $delegate.info.logs = $delegate.info.logs || [];
+    $delegate.warn.logs = $delegate.warn.logs || [];
+    $delegate.error.logs = $delegate.error.logs || [];
+  }
 
-      loggingMethods.forEach(function decorateLogging(logLevel) {
-        var original = $delegate[logLevel];
-        $delegate[logLevel] = function logToApiDecorator() {
-          var args = [].slice.call(arguments);
-          original.apply($delegate, args);
-          logToApi(args[messageIndex], logLevel);
-        };
-      });
+  decorate();
 
-      // Special... only needed to support angular-mocks expectations.
-      $delegate.info.logs = $delegate.info.logs || [];
-      $delegate.warn.logs = $delegate.warn.logs || [];
-      $delegate.error.logs = $delegate.error.logs || [];
-    }
+  return $delegate;
+};
 
-    decorate();
-
-    return $delegate;
+function provide($provide, logApiProvider) {
+  var logDecoratorShim = function $logDecorator($delegate) {
+    return logDecorator($delegate, logApiProvider);
   };
   $provide.decorator('$log', [
-    '$delegate', logDecorator
+    '$delegate', logDecoratorShim
   ]);
 }
 
