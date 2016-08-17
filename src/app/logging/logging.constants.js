5180bcc529edd4246398e28b1f4e09b36c35d67a
brian wortman <bwortman@fusionalliance.com>
2016-08-17 10:58:19 -0400

    Pulling the decorator out into a constant. Still working towards testing this thing...

--- src/app/logging/logging.constants.js
+++ src/app/logging/logging.constants.js
@@ -2,8 +2,46 @@
 
 var loggingModule = require('./logging.module');
 
-loggingModule.constant('xhr', {
-  create: function() {
-    return new XMLHttpRequest();
-  }
-});
+loggingModule
+  .constant('xhr', {
+    create: function() {
+      return new XMLHttpRequest();
+    }
+  })
+  .constant('logDecorator', {
+    decorateLogService: function(logService, logApiProvider) {
+      function logToApi(message, severity) {
+        // Do NOT use $http in the context of logging because it creates a circular dependency, initiates the digest
+        // loop, and makes logging to the API impossible if Angular itself if hosed.
+        var payload = {
+          message: message,
+          level: severity,
+          username: 'bill'
+        };
+        logApiProvider.$get().send(JSON.stringify(payload));
+      }
+
+      function decorate() {
+        var messageIndex = 0;
+        var loggingMethods = ['info', 'warn', 'error']; // NOTE: 'log' and 'debug' were deliberately skipped.
+
+        loggingMethods.forEach(function decorateLogging(logLevel) {
+          var original = logService[logLevel];
+          logService[logLevel] = function logToApiDecorator() {
+            var args = [].slice.call(arguments);
+            original.apply(logService, args);
+            logToApi(args[messageIndex], logLevel);
+          };
+        });
+
+        // Special... only needed to support angular-mocks expectations.
+        logService.info.logs = logService.info.logs || [];
+        logService.warn.logs = logService.warn.logs || [];
+        logService.error.logs = logService.error.logs || [];
+      }
+
+      decorate();
+
+      return logService;
+    }
+  });
