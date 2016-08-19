23cb54439457399da2fdecb501d8c5d117b1f055
brian wortman <bwortman@fusionalliance.com>
2016-08-19 10:30:56 -0400

    WIP - trying to get sinon integrated. Having problems with log-api.provider.spec.js

--- src/app/logging/log-api.provider.spec.js
+++ src/app/logging/log-api.provider.spec.js
@@ -1,10 +1,19 @@
+// var sinon = require('sinon');
+
 describe('fa.logging.logApi > ', function describeImpl() {
+  // var fakeServer;
   var logApi;
   beforeEach(angular.mock.module('app'));
 
   beforeEach(inject(function injectImpl(_logApi_) {
     logApi = _logApi_;
+
+    // fakeServer = sinon.fakeServer.create();
   }));
+
+  // afterEach(function afterEach() {
+    // fakeServer.restore();
+  // });
 
   it('should post message to API', function shouldPostToApi() {
     // TODO: use the sinon fake service to test an ajax post...
