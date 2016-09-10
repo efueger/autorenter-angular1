a95d3c5852560aeb3f8d2a422ee32660f6164a0d
brian wortman <bwortman@fusionalliance.com>
2016-09-10 15:53:15 -0400

    Improved notification in the httpErrorHandler.

--- src/app/error-handling/error-handling.config.js
+++ src/app/error-handling/error-handling.config.js
@@ -3,7 +3,6 @@
 var errorHandling = require('./error-handling.module');
 
 var config = function($httpProvider) {
-  console.log('TODO: Remove this log of the httpErrorHandlerFactory being configured as an interceptor!!!');
   $httpProvider.interceptors.push('httpErrorHandlerFactory');
 };
 
--- src/app/error-handling/http-error-handler.factory.js
+++ src/app/error-handling/http-error-handler.factory.js
@@ -3,14 +3,50 @@
 var errorHandling = require('./error-handling.module');
 
 var httpErrorHandlerFactory = function httpErrorHandlerFactory($q, notificationService) {
+  var getNotificationMessage = function getNotificationMessage(errorCode, data) {
+    if (errorCode === 400) {
+      return {
+        userMessage: data.message,
+        title: 'Validation error'
+      };
+    }
+
+    if (errorCode === 413) {
+      return {
+        userMessage: 'The file could not be uploaded because it is too large for the system to handle.'
+          + ' Please contact technical support.',
+        title: 'Uploaded file is too large'
+      };
+    }
+
+    if (errorCode >= 500 && errorCode < 600) {
+      return {
+        userMessage: 'The server is unavailable. Please try again.'
+          + ' If the problem persists, please notify technical support.'
+      };
+    }
+
+    if (errorCode === 404) {
+      return {
+        userMessage: 'The data you are requesting does not exist.',
+        title: 'Document not found'
+      };
+    }
+
+    if (errorCode === 401 || errorCode === 403) {
+      // Handled by authorization module
+      return null;
+    }
+
+    return {
+      userMessage: 'TODO - meaningful error message'
+    };
+  };
+
   return {
     responseError: function(response) {
-      console.log('response.status = ' + response.status);
-      notificationService.notifyError({
-        userMessage: 'Oops, an error!',
-        technicalMessage: 'oops! status = ' + response.status
-      });
-
+      var notificationData = getNotificationMessage(response.status, response.data);
+      notificationService.notifyError(notificationData);
       return $q.reject(response);
     }
   };
