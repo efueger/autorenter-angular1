9020696cabfdff57905be6b86e7225849eef2654
Jacob King <jking@fusionalliance.com>
2016-10-10 10:22:37 -0400

    Initial creation of fleet tests spec file.

---
+++ src/app/fleet/fleet-locations.controller.spec.js
@@ -0,0 +1,19 @@
+describe('fa.confirmations.FleetLocationsController > ', function describeImpl() {
+  var controller;
+  var siteIdString = "abc";
+
+  beforeEach(angular.mock.module('fa.fleet'));
+
+  beforeEach(function beforeEach() {
+    angular.mock.inject([
+      '$controller',
+      function assignController($controller) {
+        controller = $controller('FleetLocationsController', {
+            'locationsDataService': locationsDataService,
+            'confirmationService': confirmationService,
+            'strings': siteIdString
+        });
+      }
+    ]);
+  });
+});
