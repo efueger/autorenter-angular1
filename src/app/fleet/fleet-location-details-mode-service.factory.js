816277adba36335bfa3422df8b9c013a03c8a20e
brian wortman <bwortman@fusionalliance.com>
2016-11-16 13:22:34 -0500

    Improved the fleetLocationDetailsModeService tests, and fixed the broken isViewMode test.

--- src/app/fleet/fleet-location-details-mode-service.factory.js
+++ src/app/fleet/fleet-location-details-mode-service.factory.js
@@ -3,30 +3,31 @@
 var fleet = require('./fleet.module');
 
 var fleetLocationDetailsModeService = function fleetLocationDetailsModeService($state) {
+  var fleetLocationDetailsModeServiceInstance;
+
   function getNavigationStateName() {
-    console.info('in getNavigationStateName(), returning ' + $state.current.name);
     return $state.current.name;
   }
 
   function isAddMode() {
-    return getNavigationStateName() === 'fleet.locations.add';
+    return fleetLocationDetailsModeServiceInstance.getNavigationStateName() === 'fleet.locations.add';
   }
 
   function isEditMode() {
-    return getNavigationStateName() === 'fleet.locations.edit';
+    return fleetLocationDetailsModeServiceInstance.getNavigationStateName() === 'fleet.locations.edit';
   }
 
   function isViewMode() {
-    return getNavigationStateName() === 'fleet.locations.view';
+    return fleetLocationDetailsModeServiceInstance.getNavigationStateName() === 'fleet.locations.view';
   }
 
-  console.info('returning a fresh fleetLocationDetailsModeService instance...');
-  return {
+  fleetLocationDetailsModeServiceInstance = {
     isAddMode: isAddMode,
     isEditMode: isEditMode,
     isViewMode: isViewMode,
     getNavigationStateName: getNavigationStateName
   };
+  return fleetLocationDetailsModeServiceInstance;
 };
 
 fleetLocationDetailsModeService.$inject = [
--- src/app/fleet/fleet-location-details-mode-service.factory.spec.js
+++ src/app/fleet/fleet-location-details-mode-service.factory.spec.js
@@ -17,44 +17,49 @@ describe('fa.fleet.fleetLocationDetailsModeService > ', function describeImpl() 
 
   describe('isAddMode', function isAddModeTest() {
     it('returns false', function testImpl() {
-      $state.current = { name: 'fleet.locations.add.foo' };
+      fleetLocationDetailsModeService.getNavigationStateName = function() {
+        return 'fleet.locations.add.foo';
+      };
       fleetLocationDetailsModeService.isAddMode().should.be.false;
     });
 
     it('returns true', function testImpl() {
-      $state.current = { name: 'fleet.locations.add' };
+      fleetLocationDetailsModeService.getNavigationStateName = function() {
+        return 'fleet.locations.add';
+      };
       fleetLocationDetailsModeService.isAddMode().should.be.true;
     });
   });
 
   describe('isEditMode', function isEditModeTest() {
     it('returns false', function testImpl() {
-      $state.current = { name: 'fleet.locations.edit.foo' };
+      fleetLocationDetailsModeService.getNavigationStateName = function() {
+        return 'fleet.locations.edit.foo';
+      };
       fleetLocationDetailsModeService.isEditMode().should.be.false;
     });
 
     it('returns true', function testImpl() {
-      $state.current = { name: 'fleet.locations.edit' };
+      fleetLocationDetailsModeService.getNavigationStateName = function() {
+        return 'fleet.locations.edit';
+      };
       fleetLocationDetailsModeService.isEditMode().should.be.true;
     });
   });
 
   describe('isViewMode', function isViewModeTest() {
     it('returns false', function testImpl() {
-      $state.current = { name: 'fleet.locations.view.foo' };
+      fleetLocationDetailsModeService.getNavigationStateName = function() {
+        return 'fleet.locations.view.foo';
+      };
       fleetLocationDetailsModeService.isViewMode().should.be.false;
     });
 
-    it.only('returns true', function testImpl() {
+    it('returns true', function testImpl() {
       fleetLocationDetailsModeService.getNavigationStateName = function() {
         return 'fleet.locations.view';
       };
-      console.info('getNavigationStateName value from test = ' +
-        fleetLocationDetailsModeService.getNavigationStateName());
-
-      var isViewMode = fleetLocationDetailsModeService.isViewMode();
-      console.info('isViewMode = ' + isViewMode);
-      // isViewMode.should.be.true;
+      fleetLocationDetailsModeService.isViewMode().should.be.true;
     });
   });
 });
